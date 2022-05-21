0f9af5f4590206395e47ee4ce8f2f6afe732a18a
Adrian Mazur <muzy1125@gmail.com>
2022-05-21 11:41:08 +0200

    first initial config commit

---
+++ src/main/java/pl/mazi85/measurementserver/MeasurementServerApplication.java
@@ -0,0 +1,13 @@
+package pl.mazi85.measurementserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeasurementServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mazi85/measurementserver/MeasurementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mazi85.measurementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeasurementServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
